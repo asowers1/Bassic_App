c5cfd9c3ab4cf69236e59ae3612a43a831357ed6
asowers1 <asow123@gmail.com>
2015-02-04 12:27:08 -0500

    songModel class
    Added properties and an init method to the songleModel class

--- Bassic/songModel.swift
+++ Bassic/songModel.swift
@@ -7,6 +7,22 @@
 //
 
 import Foundation
+import UIKit
 
-
-// test
+class songModel: NSObject {
+    var title:String     = ""
+    var artist:String    = ""
+    var album:String     = ""
+    var length:Int       = 0
+    var albumArt:UIImage
+    
+    init(title:String, artist:String, album:String, length:Int, albumArt:UIImage) {
+        self.title    = title
+        self.artist   = artist
+        self.album    = album
+        self.length   = length
+        self.albumArt = albumArt
+    }
+    
+    
+}
